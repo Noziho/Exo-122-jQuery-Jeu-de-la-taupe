15b4bfc77079503c1c9bb2bc11747e37add0ae29
Noah2211 <noah.decroix3@gmail.com>
2021-12-21 11:53:26 +0100

    almost done

--- script.js
+++ script.js
@@ -1,3 +1,42 @@
-const holes = // utilier la méthode jquery appropriée pour selectionner les éléments ayant pour classe "hole";
-const scoreBoard = //utiliser la méthode jquery appropriée pour selectionner l'élément ayant pour classe "score";
-const moles = // utilier la méthode jquery appropriée pour selectionner les éléments ayant pour classe "mole";
+const holes = $('.hole');
+const scoreBoard = $('.score');
+const moles = $('.mole');
+
+
+function startGame() {
+
+    let holeRandom;
+    let random;
+    let min = Math.ceil(200);
+    let max = Math.floor(1000);
+    let randomTime = Math.floor(Math.random() * (max - min) + min);
+
+
+    let compteur = 0;
+    $('#startGame').click(function (){
+        let timer = 10;
+        setInterval(function (){
+            if (timer > 0) {
+                timer--;
+            }
+        }, 1000)
+
+        setInterval(function () {
+            if (timer > 0) {
+                random = Math.trunc(Math.random() * 8);
+                holeRandom = $(`.hole${random}`);
+                holeRandom.addClass('up');
+            }
+        }, randomTime);
+
+        let mole = $('.mole');
+        mole.click(function () {
+            compteur++;
+            $('.score').html(compteur);
+            holeRandom.removeClass('up');
+        })
+    })
+
+}
+
+startGame();
